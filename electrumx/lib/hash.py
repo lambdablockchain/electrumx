3578b69f7783a1ea260fa0e6e2f4f10f9c7a22b8
Mehdi <105603194+Ektisad25@users.noreply.github.com>
2024-10-14 22:16:00 +0200

    Update hash.py

--- electrumx/lib/hash.py
+++ electrumx/lib/hash.py
@@ -1,4 +1,54 @@
+# This license can be used by developers, projects or companies who wish to make their software or applications available for open (free) usage only on the Lambda Blockchains.
+# 
+# Copyright (c) 2022 The Lambda Blockchain Developers
+# 
+# Open Lambda Blockchain (RAD) License Version 1
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#  
+# 1 - The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# 2 - The Software, and any software that is derived from the Software or parts thereof,
+# can only be used on the Lambda (RAD) blockchains. The Lambda (RAD) blockchains are defined,
+# for purposes of this license, as the Lambda (RAD) blockchains containing block height #10,459
+# with the hash "0000000000229980bb7bcf653ebb94d6ffe18fed6cd3a3a98b876312414cb831" and that 
+# contains the longest persistent chain of blocks accepted by this Software, as well as the test 
+# blockchains that contain the longest persistent chains of blocks accepted by this Software.
+# 
+# 3 - The Software, and any software that is derived from the Software or parts thereof, can only
+# be used on Lambda (RAD) blockchain that maintains the original difficulty adjustment algorithm, 
+# maintains the block subsidy emission rate defined in the original version of this Software, and 
+# ensures all coins are spendable in the normal manner without either subverting, undermining, 
+# changing, diminishing, nullifying, hijacking, or altering the way existing coins can be spent. Any 
+# attempt or proposal by an entity to violate, change, or remove the logic for verifying 
+# digital chains of signatures for existing coins will be deemed a violation of this license 
+# and that entity must cease to use the Software immediately.
+# 
+# 4 - Users and providers of the Software agree to insure themselves against any loss of any kind
+# if they wish to mitigate the effects of theft or error. The Users and providers agree
+# and understand that under no circumstances will there be recourse through Lambda (RAD) blockchain
+# providers via subverting, undermining, changing, diminishing, nullifying, hijacking, or altering 
+# the way existing coins can be spent and the proper functioning of the verification of chains of 
+# digital signatures.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+# Previous version of the software are copyright and licensed as follows:
+
 # Copyright (c) 2016-2017, Neil Booth
+# Copyright (c) 2017, the ElectrumX authors
 #
 # All rights reserved.
 #
@@ -22,6 +72,8 @@
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# and warranty status of this software.
+
 
 '''Cryptograph hash functions and related classes.'''
 
@@ -30,16 +82,14 @@ import hashlib
 import hmac
 
 from electrumx.lib.util import bytes_to_int, int_to_bytes, hex_to_bytes
-
+from Crypto.Hash import SHA512
 _sha256 = hashlib.sha256
 _new_hash = hashlib.new
 _new_hmac = hmac.new
 HASHX_LEN = 11
 
 
-def sha256(x):
-    '''Simple wrapper of hashlib sha256.'''
-    return _sha256(x).digest()
+
 
 
 def ripemd160(x):
@@ -49,19 +99,22 @@ def ripemd160(x):
     return h.digest()
 
 
+def sha256(x):
+    '''SHA-256 hash of the input.'''
+    # Create a new SHA-256 hash object
+    firsthash = hashlib.sha256()
+    # Update the hash object with the input data
+    firsthash.update(x)
+    # Return the digest (the hash value)
+    return firsthash.digest()
+
+
+
 def double_sha256(x):
-    '''SHA-256 of SHA-256, as used extensively in bitcoin.'''
-    return sha256(sha256(x))
-
-
-def tagged_sha256(x):
-    h = sha256("PoW".encode('utf-8'))
-    return sha256(h + h + x)
-
-
-def tagged_sha256d(x):
-    return tagged_sha256(tagged_sha256(x))
-
+    '''SHA-256 of SHA-256, as used extensively in Bitcoin and Bitcoin Cash.'''
+    first_hash = sha256(x)      # First SHA-256
+    second_hash = sha256(first_hash)  # Second SHA-256
+    return second_hash
 
 def hash_to_hex_str(x):
     '''Convert a big-endian binary hash to displayed hex string.
